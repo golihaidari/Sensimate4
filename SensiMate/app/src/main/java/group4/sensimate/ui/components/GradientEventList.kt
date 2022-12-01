7c7621c3e22d102005ce52625fb96f09f58b295e
goli <golbashaidari@gmail.com>
2022-12-01 09:28:49 +0100

    I just changed a bit welcomeScreen functionality.

--- SensiMate/app/src/main/java/group4/sensimate/ui/components/GradientEventList.kt
+++ SensiMate/app/src/main/java/group4/sensimate/ui/components/GradientEventList.kt
@@ -116,7 +116,7 @@ fun EventCard(event: Event, page: String, navController: NavController){
                     val context = LocalContext.current
                     OutlinedButton(
                         onClick = {
-                            context.startActivity(Intent(context, SurveyActivity::class.java))
+                            //context.startActivity(Intent(context, SurveyActivity::class.java))
                         },
                         modifier = Modifier
                             .fillMaxWidth()
@@ -129,8 +129,8 @@ fun EventCard(event: Event, page: String, navController: NavController){
                 }else{
                     OutlinedButton(
                         onClick = {
-                            navController.navigate(SurveyDetailsScreen.LaunchSurvey.route)
-                            //navController.navigate(SurveyDetailsScreen.ScanBarCode.route)
+                            //navController.navigate(SurveyDetailsScreen.LaunchSurvey.route)
+                            navController.navigate(SurveyDetailsScreen.ScanBarCode.route)
                         },
                         modifier = Modifier
                             .fillMaxWidth()
